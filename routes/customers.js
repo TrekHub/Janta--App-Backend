90b0bfc45f3efe252e42904a0b808febeeeb7b7a
TrekHub <teddyomondi001@gmail.com>
2022-03-17 19:07:59 +0300

    creating the middleware for finding customers with their respective ids

--- routes/customers.js
+++ routes/customers.js
@@ -21,21 +21,107 @@ router.get('/', async (req, res) => {
 });
 
 //Get one customer endpoint
-router.get('/:id', (req, res) => {
+router.get('/:id', getCustomerById, (req, res) => {
+    res.send(res.customer);
 
 })
 
 
 //Create one customer endpoint
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const customer = new Customer({
+        name: req.body.name,
+        email: req.body.email,
+        phoneNo: req.body.phoneNo,
+        location: req.body.location
+
+    });
+
+    try {
+        const newCustomer = await customer.save();
+        res.status(201).json(newCustomer);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+
+    }
 
 })
 
 
 //Update one customer route
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getCustomerById, async (req, res) => {
+    if (req.body.name != null) {
+        res.customer.name = req.body.name
+    }
+
+    if (req.body.email != null) {
+        res.customer.email = req.body.email
+    }
+
+    if (req.body.phoneNo != null) {
+        res.customer.phoneNo = req.body.phoneNo
+    }
+
+    if (req.body.location != null) {
+        res.customer.location = req.body.location
+    }
+
+
+    try {
+
+        const updateCustomer = await res.customer.save();
+        res.json(updateCustomer)
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+
+    }
 
 })
+
+
+//Delete a specific custome 
+router.delete('/:id', getCustomerById, async (req, res) => {
+    try {
+        await res.customer.remove();
+        res.json({
+            message: "Successful Deleted The Customer"
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+})
+
+
+
+//Getting Customers by their Ids middleware
+async function getCustomerById(req, res, next) {
+    let customer;
+
+    try {
+        customer = await Customer.findById(req.params.id)
+        if (customer == null) {
+            return res.status(404).json({
+                message: "Cannot Find the Customer"
+            });
+        }
+    } catch (error) {
+
+        return res.status(500).json({
+            message: error.message
+        });
+
+    }
+    res.customer = customer;
+    next();
+}
 
 
 module.exports = router;
